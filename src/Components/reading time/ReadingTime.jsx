352ecabce3116dd7368898863d931504bb8849b3
sagar-ghosh1 <sagorghosh520@gmail.com>
2023-04-10 21:19:07 +0600

    readingtime2

--- src/Components/reading time/ReadingTime.jsx
+++ src/Components/reading time/ReadingTime.jsx
@@ -4,8 +4,8 @@ import "./redingTime.css"
 const ReadingTime = ({ readingTime }) => {
     return (
         <div>
-            <div className='border-2 border-blue-500 p-2'>
-                <h2 className='text-[15px] md:text-2xl text-center font-bold text-blue-600'>Spent time on read : {readingTime} min</h2>
+            <div className='border-2 border-purple-500 p-2'>
+                <h2 className='text-[15px] md:text-2xl text-center font-bold text-purple-600'>Spent time on read : {readingTime} min</h2>
             </div>
         </div>
     );
